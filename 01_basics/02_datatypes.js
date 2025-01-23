b1c5b9bf7317e04b0c98e8986ea210235a2ce6a4
amitparab-op <parabamit052@gmail.com>
2025-01-23 05:47:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert(2 + 5) we are using nodejs not browser
+
+console.log(2 + 5)
+
+console.log("Amit")
+
+let name = "Amit"
+let age = 23
+let isLoggedIn = true
+let state;
+
+// primitive data types
+// number >= 2 to power 53
+// BigInt
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but value not declared
+// symbol => unique
+
+//objects
+console.log(typeof "Amit"); // string
+console.log(typeof age); //number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
